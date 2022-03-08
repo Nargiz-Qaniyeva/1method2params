81e40625bb7cd6f094eedb9f2fd2a79bc0da5491
Nargiz-Qaniyeva <tu7lyw21z@code.edu.az>
2022-03-08 19:49:47 +0400

    first commit

---
+++ metodparams/metodparams/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace metodparams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+
+
+            int math= division (15,5);
+            Console.WriteLine(math );
+            
+            #endregion
+        }
+        static int division (int num1, int num2)
+        {
+            return num1/num2;
+        }
+    }
+}
